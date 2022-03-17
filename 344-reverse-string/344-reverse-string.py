85e3bd57a7cffcb9fc624278cdbce10100663a12
wndudrla1011 <wndudrla1011@naver.com>
2022-03-17 20:27:42 +0900

    Time: 176 ms (78.31%), Space: 21 MB (79.06%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,3 +1,8 @@
 class Solution:
     def reverseString(self, s):
-        s.reverse()
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+
+            left += 1
+            right -= 1
