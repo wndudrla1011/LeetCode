327aa5d6c3e272fb73751ec521910200a41e51c0
wndudrla1011 <wndudrla1011@naver.com>
2022-03-22 10:37:19 +0900

    Time: 44 ms (94.46%), Space: 14.5 MB (23.69%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,8 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i, n in enumerate(nums):
-            complement = target - n
+        nums_map = {}
 
-            if complement in nums[i + 1:]:
-                return [nums.index(n), nums[i+1:].index(complement) + (i + 1)]
+        for i, num in enumerate(nums):
+            nums_map[num] = i
+
+        for i, num in enumerate(nums):
+            if target - num in nums_map and i != nums_map[target - num]:
+                return [i, nums_map[target - num]]
