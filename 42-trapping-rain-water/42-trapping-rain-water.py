72f3e5d74cd18a6c41d91f1440c06719726ec004
wndudrla1011 <wndudrla1011@naver.com>
2022-03-22 16:25:09 +0900

    Time: 84 ms (68.98%), Space: 15.2 MB (20.42%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        stack = []
         volume = 0
-        left, right = 0, len(height) - 1
-        left_max, right_max = height[left], height[right]
 
-        while left < right:
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
 
-            if left_max <= right_max:
-                volume += left_max - height[left]
-                left += 1
-            else:
-                volume += right_max - height[right]
-                right -= 1
+                if not len(stack):
+                    break
+
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+
+            stack.append(i)
 
         return volume
