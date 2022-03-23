abec5bcceb846c23cc1ea24caddd60c3cb979c7d
wndudrla1011 <wndudrla1011@naver.com>
2022-03-23 12:50:35 +0900

    Time: 74 ms (63.47%), Space: 13.1 MB (97.05%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        left, right = 0, len(x) - 1
+
+        while left < right:
+            if x[left] != x[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
