8d57f8cab4e4fb0548ef4221db91287f48c99fa1
wndudrla1011 <wndudrla1011@naver.com>
2022-03-23 12:41:35 +0900

    Time: 373 ms (32.17%), Space: 15.6 MB (13.26%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.py
+++ 561-array-partition-i/561-array-partition-i.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return sum(sorted(nums)[::2])
+        pair, n = 2, 0
+        sum = 0
+        nums.sort()
+
+        while pair * n != len(nums):
+            sum += min(nums[pair * n], nums[pair * n + 1])
+            n += 1
+
+        return sum
