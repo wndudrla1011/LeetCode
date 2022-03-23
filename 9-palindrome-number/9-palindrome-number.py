8484a9e866f585f142bf11c2bafcd844dc6f49f9
wndudrla1011 <wndudrla1011@naver.com>
2022-03-23 12:53:26 +0900

    Time: 94 ms (37.30%), Space: 13.2 MB (97.05%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -5,12 +5,4 @@ class Solution(object):
         :rtype: bool
         """
         x = str(x)
-        left, right = 0, len(x) - 1
-
-        while left < right:
-            if x[left] != x[right]:
-                return False
-            left += 1
-            right -= 1
-
-        return True
+        return x == x[::-1]
