b63d7f1da9c1535390e025369a7a71baf411c954
wndudrla1011 <wndudrla1011@naver.com>
2022-03-28 10:24:40 +0900

    Time: 684 ms (74.70%), Space: 16.6 MB (77.89%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -7,15 +7,12 @@ class Solution(object):
         result = []
         nums.sort()
 
-        # i -> standard
         for i in range(len(nums) - 2):
-
-            # filtering the same combination
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             left, right = i + 1, len(nums) - 1
 
-            # searching a case that satisfies the conditions
             while left < right:
                 sum = nums[i] + nums[left] + nums[right]
                 if sum < 0:
@@ -25,7 +22,6 @@ class Solution(object):
                 else:
                     result.append([nums[i], nums[left], nums[right]])
 
-                    # filtering the same combination
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
