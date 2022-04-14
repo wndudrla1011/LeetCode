db965e92a7a3d4345b7a5b50e3d9e6cf9228d4ce
wndudrla1011 <wndudrla1011@naver.com>
2022-04-14 10:28:29 +0900

    Time: 23 ms (98.36%), Space: 16.7 MB (94.38%) - LeetHub

---
+++ 328-odd-even-linked-list/328-odd-even-linked-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        node, odd, even = head, ListNode(None), ListNode(None)
+        odd_list, even_list = odd, even
+
+        number, cnt = 1, 0
+        while node:
+            if number % 2 != 0:
+                odd.next, next = node, node.next
+                odd = odd.next
+                cnt += 1
+            else:
+                even.next, next = node, node.next
+                even = even.next
+            node = next
+            number += 1
+        else:
+            if number % 2 != 0:
+                odd.next = node
+            else:
+                even.next = node
+
+        result = odd_list.next
+        while cnt:
+            odd_list = odd_list.next
+            cnt -= 1
+
+        odd_list.next = even_list.next
+
+        return result
