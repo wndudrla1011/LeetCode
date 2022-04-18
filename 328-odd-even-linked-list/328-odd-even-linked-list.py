98ff2ea81498e13c1d416662a7777a476215debb
wndudrla1011 <wndudrla1011@naver.com>
2022-04-18 11:15:22 +0900

    Time: 31 ms (87.78%), Space: 17 MB (57.24%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.py
+++ 328-odd-even-linked-list/328-odd-even-linked-list.py
@@ -15,25 +15,23 @@ class Solution(object):
         number, cnt = 1, 0
         while node:
             if number % 2 != 0:
-                odd.next, next = node, node.next
+                odd.next = node
                 odd = odd.next
                 cnt += 1
             else:
-                even.next, next = node, node.next
+                even.next = node
                 even = even.next
-            node = next
+            node = node.next
             number += 1
         else:
-            if number % 2 != 0:
-                odd.next = node
-            else:
-                even.next = node
-
-        result = odd_list.next
-        while cnt:
-            odd_list = odd_list.next
-            cnt -= 1
+            if odd.next:
+                odd.next = None
+            if even.next:
+                even.next = None
+            result = odd_list
+            while cnt:
+                odd_list = odd_list.next
+                cnt -= 1
 
         odd_list.next = even_list.next
-
-        return result
+        return result.next
