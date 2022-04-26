1837ae2c8623be37ef9636e213ad4199bb686b0a
wndudrla1011 <wndudrla1011@naver.com>
2022-04-26 16:52:28 +0900

    Time: 64 ms (75.35%), Space: 13.9 MB (82.99%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,19 +1,11 @@
 class MyCircularQueue(object):
 
     def __init__(self, k):
-        """
-        :type k: int
-        """
         self.q = [None] * k
         self.maxlen = k
-        self.p1 = 0  # front
-        self.p2 = 0  # rear
+        self.p1, self.p2 = 0, 0
 
     def enQueue(self, value):
-        """
-        :type value: int
-        :rtype: bool
-        """
         if self.q[self.p2] is None:
             self.q[self.p2] = value
             self.p2 = (self.p2 + 1) % self.maxlen
@@ -22,9 +14,6 @@ class MyCircularQueue(object):
             return False
 
     def deQueue(self):
-        """
-        :rtype: bool
-        """
         if self.q[self.p1] is None:
             return False
         else:
@@ -33,28 +22,18 @@ class MyCircularQueue(object):
             return True
 
     def Front(self):
-        """
-        :rtype: int
-        """
         return -1 if self.q[self.p1] is None else self.q[self.p1]
 
     def Rear(self):
-        """
-        :rtype: int
-        """
         return -1 if self.q[self.p2 - 1] is None else self.q[self.p2 - 1]
 
     def isEmpty(self):
-        """
-        :rtype: bool
-        """
         return self.p1 == self.p2 and self.q[self.p1] is None
 
     def isFull(self):
-        """
-        :rtype: bool
-        """
         return self.p1 == self.p2 and self.q[self.p1] is not None
+
+    
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
