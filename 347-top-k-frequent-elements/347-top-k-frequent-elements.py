c7ece67c15753708fa821fbf176910cdd7ed9ca0
wndudrla1011 <wndudrla1011@naver.com>
2022-04-30 11:41:30 +0900

    Time: 144 ms (29.33%), Space: 16.7 MB (71.71%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -5,14 +5,6 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        freqs = collections.Counter(nums)
-        freqs_heap = []
-
-        for f in freqs:
-            heapq.heappush(freqs_heap, (-freqs[f], f))
-
-        topk = []
-        for _ in range(k):
-            topk.append(heapq.heappop(freqs_heap)[1])
+        
                 
-        return topk
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
