7fd925267df16bfc33ece0669fbf67f1c83592c8
wndudrla1011 <wndudrla1011@naver.com>
2022-05-11 21:24:28 +0900

    Time: 47 ms (19.75%), Space: 13.7 MB (39.50%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -4,5 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        permutations = []
+        prev_elements = []
 
-        return list(map(list, itertools.permutations(nums)))
+        def dfs(elements):
+            if len(elements) == 0:
+                permutations.append(prev_elements[:])
+
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+
+
+        dfs(nums)
+        
+        return permutations
