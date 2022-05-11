04a329dc5e308b8dd15208126ff1e0d272b3fc5f
wndudrla1011 <wndudrla1011@naver.com>
2022-05-11 21:17:01 +0900

    Time: 23 ms (92.45%), Space: 13.7 MB (39.50%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        permutations = []
+        prev_elements = []
+
+        def dfs(elements):
+            if len(elements) == 0:
+                permutations.append(prev_elements[:])
+
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+
+
+        dfs(nums)
+        
+        return permutations
