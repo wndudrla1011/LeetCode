79786af9a3d5b6afdd9c32bf3c15257882d99a2c
wndudrla1011 <wndudrla1011@naver.com>
2022-05-12 20:34:22 +0900

    Time: 7576 ms (5.05%), Space: 15.1 MB (33.91%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -5,4 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        return list(itertools.combinations(range(1, n + 1), k))
+        arr = [0 for i in range(n+1)]
+
+        result = []
+
+        def dfs(v, visited = []):
+            if collections.Counter(arr)[1] == k:
+                result.append(visited[:])
+                return
+
+            for i in range(v, n+1):
+                if i not in visited:
+                    visited.append(i)
+                    arr[i] = 1
+                dfs(i+1, visited)
+                visited.pop()
+                arr[i] = 0
+
+        dfs(1, [])
+        
+        return result
