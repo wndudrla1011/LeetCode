c86a6d65a33f3f72d908ca5c4683c22bf6de60a2
wndudrla1011 <wndudrla1011@naver.com>
2022-05-13 15:24:44 +0900

    Time: 99 ms (43.13%), Space: 13.6 MB (39.35%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result = []
+
+
+        def dfs(elem, v):
+            if sum(elem) == target:
+                result.append(elem[:])
+                return
+            elif sum(elem) > target:
+                return
+
+            for i in range(v, len(candidates)):
+                elem.append(candidates[i])
+                dfs(elem, i)
+                elem.pop()
+
+
+        dfs([], 0)
+        
+        return result
