dd07fb1fe4a7a7e0d45f6885f20b1cc73a4c7ba1
wndudrla1011 <wndudrla1011@naver.com>
2022-05-19 21:31:31 +0900

    Time: 143 ms (57.55%), Space: 15 MB (41.82%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+        graph = collections.defaultdict(list)
+
+        # create a graph
+        for s, d, c in flights:
+            graph[s].append((d, c))
+
+        visited = {}
+
+        # Q = [(cost, source)]
+        Q = [(0, src, 0)]
+
+        # via = transferred count
+        while Q:
+            cost, node, via = heapq.heappop(Q)
+            # arrived dst
+            if node == dst:
+                return cost
+            if node not in visited or visited[node] > via:
+                visited[node] = via
+                # transfer k+1 times from src
+                for d, c in graph[node]:
+                    if via <= k:
+                        alt = cost + c
+                        heapq.heappush(Q, (alt, d, via + 1))
+
+        return -1
